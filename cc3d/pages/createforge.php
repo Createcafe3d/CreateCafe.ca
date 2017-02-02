77a6222a2ed9dc3ed19de8ca6ce5241d0f23c463
Rameez Virji <rameezv@bigtreeworld.com>
2017-02-02 00:05:51 -0600

    Clean up ampersands in createforge

--- cc3d/pages/createforge.php
+++ cc3d/pages/createforge.php
@@ -19,7 +19,7 @@
       <img src="images/stand.png" alt="Headphone Stand"/>
       <h2>Create Forge Products</h2>
       <div class="slide">
-        <h2>Custom Personal & Team Products</h2>
+        <h2>Custom Personal &amp; Team Products</h2>
         <div>
           Create Forge can supply custom personal or team merchandise, ideal for gifts or promotional branding.
           <br /><br />All custom merchandise requires a 3D logo file to be created (one time cost $65), This 3d file can then be used to make additional custom merchandise with any of our products.
@@ -29,9 +29,9 @@
     </div>
     <div class="card">
       <img src="images/stand-group.png" alt="Headphone Stand"/>
-      <h2>Customized Personal & Team Products</h2>
+      <h2>Customized Personal &amp; Team Products</h2>
       <div class="slide">
-        <h2>Customized Personal & Team Products</h2>
+        <h2>Customized Personal &amp; Team Products</h2>
         <div>
           Create Forge is devoted to providing consumers, teams and streamers with affordable custom branded products. Our products can be bought in a wide variety of colors & branding options.
           <br /><br /><a class="button" href="shop/product/10-custom-branded-gaming-headphone-stand-team-orders-only/">Team Products</a>
@@ -44,7 +44,7 @@
       <div class="slide">
         <h2>Wholesale Purchases</h2>
         <div>
-         Our team will ensure that you get the product edge for your shop with function & quality. Using our experience in design and manufacturing we are able to create any product that you may wish to bring into your store even outside of what we offer on our site.  
+         Our team will ensure that you get the product edge for your shop with function &amp; quality. Using our experience in design and manufacturing we are able to create any product that you may wish to bring into your store even outside of what we offer on our site.  
           <br /><br /><a class="button" href="mailto:createforge@createcafe.ca" target="_blank">Contact CreateForge</a>
         </div>
       </div>
@@ -53,12 +53,12 @@
   <div class="section dark">
     <div class="content">
       <div class="logo">
-        <img src="media/createforge_glow.png" alt="CreateForge"/><h2>Create<b>Forge</b></h1>
+        <img src="media/createforge_glow.png" alt="CreateForge"/><h2>Create<b>Forge</b></h2>
       </div>
-      CREATE<b>FORGE</b> is devoted to providing teams and streamers with custom branded products. Our products can be bought in a wide variety of colors & design features
+      CREATE<b>FORGE</b> is devoted to providing teams and streamers with custom branded products. Our products can be bought in a wide variety of colors &amp; design features
      <br /><br />
       Our team has many years of experience in 3D Modeling and 3D Printing and would like to ensure that you get the market edge for your team, clan or streaming shop with these functional quality products.
-  ​   <br /><br />
+     <br /><br />
     <a href="/shop/product/3d-logo-creation/" class="button">Get started with a 3D Logo</a>
     </div>
 
